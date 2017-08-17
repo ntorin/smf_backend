01c2a58552ac899cc14ac2f5c039c97489ff1639
Iris Inami <irisinami@Iriss-Mac.local>
2017-08-17 03:39:40 -0700

    updating production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '52.14.199.102', user: 'deploy', roles: %w{web app db}
+server '18.220.137.59', user: 'deploy', roles: %w{web app db}
