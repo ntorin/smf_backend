1e4b4ce6c96b1899f11a4acb27cd3407bd25ba63
Iris Inami <irisinami@Iriss-Mac.local>
2017-11-29 07:49:55 -0800

    changing migrations

--- db/migrate/20171101093047_add_stats_to_user.rb
+++ db/migrate/20171101093047_add_stats_to_user.rb
@@ -1,19 +1,4 @@
 class AddStatsToUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :accountid, :string
-    add_column :users, :name, :string
-    add_column :users, :birthday, :date
-    add_column :users, :follower_count, :integer
-    add_column :users, :following_count, :integer
-    add_column :users, :friend_count, :integer
-    add_column :users, :credits, :integer
-    add_column :users, :credit_multiplier, :integer
-    add_column :users, :total_likes, :integer
-    add_column :users, :total_dislikes, :integer
-    add_column :users, :topic_count, :integer
-    add_column :users, :post_count, :integer
-    add_column :users, :group_count, :integer
-    add_column :users, :accepted_tos, :boolean
-    add_column :users, :verified, :boolean
   end
 end
