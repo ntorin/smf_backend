c457810e829ac7aceb48126e1a8a4ac54b63bacf
Iris Inami <irisinami@Iriss-Mac.local>
2017-12-24 07:17:43 -0800

    changing seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-[Group, User, Topic, Post].each do |table|
+[Group, User, Topic, Post, Friend, Follow, GroupUser].each do |table|
   ActiveRecord::Base.connection.execute("TRUNCATE #{table.table_name}")
   ActiveRecord::Base.connection.reset_pk_sequence!(table.table_name)
 end
@@ -20,8 +20,7 @@ Group.create(
     }
 )
 
-# sample data; 20 000 users, 2 000 groups with 1 500 users each, 500 topics per group, 800 posts per topic. 100 000 random follows, 50 000 random friends
-20000.times do |u|
+100.times do |u|
   User.create(
       [
           {
@@ -52,11 +51,11 @@ end
 
 Faker::UniqueGenerator.clear
 
-2000.times do |g|
+41.times do |g|
 
   group = Group.create(
       {
-          user_id: Faker::Number.between(0, 20001),
+          user_id: Faker::Number.between(0, 101),
           identifier: Faker::Lorem.unique.characters(1..16),
           name: Faker::Coffee.blend_name,
           description: Faker::Lorem.sentence,
@@ -70,27 +69,27 @@ Faker::UniqueGenerator.clear
 
   GroupUser.create(
                {
-                   group_id: g,
+                   group_id: group.id,
                    user_id: group.user_id,
                    role: 'creator'
                }
   )
 
-  1499.times do |gu|
+  15.times do |gu|
     GroupUser.create(
         {
-            group_id: g,
-            user_id: Faker::Number.between(0, 20001),
+            group_id: group.id,
+            user_id: Faker::Number.between(0, 101),
             role: Faker::Boolean.boolean(0.8) ? 'user' : Faker::Boolean.boolean(0.8) ? 'moderator' : 'admin'
         }
     )
   end
 
-  500.times do |t|
+  40.times do |t|
     topic = Topic.create(
              {
-                 group_id: g,
-                 user_id: Faker::Number.between(0, 20001),
+                 group_id: group.id,
+                 user_id: Faker::Number.between(0, 101),
                  title: Faker::Lorem.sentence,
                  topic_type: 0,
                  is_anonymous: Faker::Boolean.boolean,
@@ -102,8 +101,8 @@ Faker::UniqueGenerator.clear
     )
     Post.create(
         {
-            group_id: g,
-            topic_id: t,
+            group_id: group.id,
+            topic_id: topic.id,
             user_id: topic.user_id,
             content: Faker::Lorem.paragraph,
             likes: 0,
@@ -112,12 +111,12 @@ Faker::UniqueGenerator.clear
             is_anonymous: topic.is_anonymous
         }
     )
-    799.times do |p|
+    40.times do |p|
       Post.create(
               {
-                  group_id: g,
-                  topic_id: t,
-                  user_id: Faker::Number.between(0, 20001),
+                  group_id: group.id,
+                  topic_id: topic.id,
+                  user_id: Faker::Number.between(0, 101),
                   content: Faker::Lorem.paragraph,
                   likes: 0,
                   dislikes: 0,
@@ -129,20 +128,20 @@ Faker::UniqueGenerator.clear
   end
 end
 
-100000.times do |fo|
+100.times do |fo|
   Follow.create(
             {
-                follower_id: Faker::Number.between(0, 20001),
-                following_id: Faker::Number.between(0, 20001)
+                follower_id: Faker::Number.between(0, 101),
+                following_id: Faker::Number.between(0, 101)
             }
   )
 end
 
-50000.times do |fr|
+100.times do |fr|
   Friend.create(
             {
-                friend_one: Faker::Number.between(0, 20001),
-                friend_two: Faker::Number.between(0, 20001),
+                friend_one: Faker::Number.between(0, 101),
+                friend_two: Faker::Number.between(0, 101),
                 is_accepted: Faker::Boolean.boolean(0.2)
             }
   )
