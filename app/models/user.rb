7c6fa6cb6eb7b2b59b653371a9ebef83cc63363b
Iris Inami <irisinami@Iriss-Mac.local>
2018-02-18 11:31:45 -0800

    fixing validations; changing seeds.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ActiveRecord::Base
 
   has_many :topics
 
-  validates :identifier, format: { with: /\A[a-zA-Z0-9_]{1,16}\z/, message: 'can only contain letters, numbers, and underscores between 1-16 characters'}
-  validates :name, format: { with: /\A.{1,16}\z/, message: 'must be 1-16 characters' }
+  validates :identifier, format: { with: /\A[a-zA-Z0-9_]{1,16}\z/, message: 'can only contain letters, numbers, and underscores between 1-16 characters'}, allow_blank: true
+  validates :name, format: { with: /\A.{1,16}\z/, message: 'must be 1-16 characters' }, allow_blank: true
 
   def self.reset_daily_posts
     User.update_all(daily_post_count: 0)
