9d927c010feb33616ed5acbb2c52b3087434cbb9
Iris Inami <irisinami@Iriss-Mac.local>
2018-02-18 11:43:30 -0800

    adding accepted_tos to user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -88,6 +88,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit( :email, :identifier, :name, :blurb, :birthday )
+      params.require(:user).permit( :email, :identifier, :name, :blurb, :birthday, :accepted_tos )
     end
 end
